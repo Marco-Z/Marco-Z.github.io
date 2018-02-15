ce622d26447dfb796e9ad28984071e0bf015cf72
Marco-Z <marcozugliani94@live.com>
2018-02-15 14:07:44 +0200

    v2.0

--- sudoku/static/js/script.js
+++ sudoku/static/js/script.js
@@ -1,25 +1,53 @@
 "use strict";
 
+//STATE VARIABLES
 let points = 0;
 let startTime = new Date().getTime();
+let board = [];
 
+// SUDOKU FUNCTIONS
 let init = () => {
-	let grill = [
-	[1,1,5],[1,2,3],				[1,5,7],
-	[2,1,6],				[2,4,1],[2,5,9],[2,6,5],
-			[3,2,9],[3,3,8],								[3,8,6],
-	[4,1,8],						[4,5,6],						[4,9,3],
-	[5,1,4],				[5,4,8],		[5,6,3],				[5,9,1],
-	[6,1,7],						[6,5,2],						[6,9,6],
-			[7,2,6],								[7,7,2],[7,8,8],
-							[8,4,4],[8,5,1],[8,6,9],				[8,9,5],
-									[9,5,8],				[9,8,7],[9,9,9]
-	];
-	grill.forEach(x => {
-		let el = document.querySelector('.row'+x[0]+'.column'+x[1]);
-		el.innerHTML = '<input type="text" value="'+x[2]+'" disabled>';
-		el.className = el.className+' grey';
-	});
+  window.parent.postMessage({
+    messageType: "SETTING",
+    options: {
+      "width": 600,
+      "height": 643
+      }
+  }, "*");
+  let sudoku = document.getElementById('sudoku');
+  for(let i=1;i<=9;i++){
+    let row = document.createElement('tr');
+    let board_row = [];
+    for(let j=1;j<=9;j++){
+      let cell = document.createElement('td');
+      cell.className = "r"+i+" c"+j;
+      let input = document.createElement('input');
+      input.setAttribute("type", "number");
+      cell.appendChild(input);
+      row.appendChild(cell);
+      board_row.push(0);
+    }
+    sudoku.appendChild(row);
+    board.push(board_row);
+  }
+
+  let grill = [
+  [1,1,5],[1,2,3],                [1,5,7],
+  [2,1,6],                [2,4,1],[2,5,9],[2,6,5],
+          [3,2,9],[3,3,8],                                [3,8,6],
+  [4,1,8],                        [4,5,6],                        [4,9,3],
+  [5,1,4],                [5,4,8],        [5,6,3],                [5,9,1],
+  [6,1,7],                        [6,5,2],                        [6,9,6],
+          [7,2,6],                                [7,7,2],[7,8,8],
+                          [8,4,4],[8,5,1],[8,6,9],                [8,9,5],
+                                  [9,5,8],                [9,8,7],[9,9,9]
+  ];
+  grill.forEach(x => {
+    let el = document.querySelector('.r'+x[0]+'.c'+x[1]);
+    board[x[0]-1][x[1]-1] = x[2];
+    el.innerHTML = '<input type="text" value="'+x[2]+'" disabled>';
+    el.className = el.className+' grey';
+  });
 }
 
 Array.prototype.check = function(r1,r2,c1,c2){
@@ -33,22 +61,21 @@ Array.prototype.check = function(r1,r2,c1,c2){
 }
 
 function getBoard() {
-	let matrix = [];
-	let sudoku = document.getElementById('sudoku');
-	for (let i = 1; i <= 9; i++) {
-		let row = [];
-		for (let j = 1; j <= 9; j++) {
-			let el = document.querySelector('.row'+i+'.column'+j+'>input');
-			row.push(parseInt(el.value));
-		}
-		matrix.push(row);
-	}
-	console.log(matrix);
-	return matrix;
+  let matrix = [];
+  let sudoku = document.getElementById('sudoku');
+  for (let i = 1; i <= 9; i++) {
+    let row = [];
+    for (let j = 1; j <= 9; j++) {
+      let el = document.querySelector('.r'+i+'.c'+j+'>input');
+      row.push(parseInt(el.value));
+    }
+    matrix.push(row);
+  }
+  console.log(matrix);
+  return matrix;
 }
 
-function doneOrNot() {
-  let board = getBoard();
+function done() {
   let res = true;
   for(let i=0;i<3;i++){
     for(let j=0;j<3;j++){
@@ -59,12 +86,11 @@ function doneOrNot() {
       res = res && board.check(i,i+1,0,9);
       res = res && board.check(0,9,i,i+1);    
   }
-  // alert(res? 'Finished!' : 'Try again!')
   return res;
 }
 
-function pack(board) {
-	let p = ""
+function pack() {
+  let p = ""
   for(let i=0;i<9;i++){
     for(let j=0;j<9;j++){
       let val = parseInt(board[i][j]);
@@ -75,80 +101,85 @@ function pack(board) {
 }
 
 function unpack(b) {
-	let matrix = [];
-	let sudoku = document.getElementById('sudoku');
-	for (let i = 0; i < 9; i++) {
-		let row = [];
-		for (let j = 0; j < 9; j++) {
-			let v = parseInt(b[i*9+j]);
-			row.push(parseInt(v));
-		}
-		matrix.push(row);
-	}
-	console.log(matrix);
-	return matrix;
+  let matrix = [];
+  let sudoku = document.getElementById('sudoku');
+  for (let i = 0; i < 9; i++) {
+    let row = [];
+    for (let j = 0; j < 9; j++) {
+      let v = parseInt(b[i*9+j]);
+      row.push(parseInt(v));
+    }
+    matrix.push(row);
+  }
+  board = matrix;
+  return matrix;
 }
 
 function resume(state) {
-	let sudoku = document.getElementById('sudoku');
-	for (let i = 1; i <= 9; i++) {
-		for (let j = 1; j <= 9; j++) {
-			let val = state[i-1][j-1]
-			document.querySelector('.row'+i+'.column'+j+'>input').value = val>0?val:'';
-		}
-	}
+  let sudoku = document.getElementById('sudoku');
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      let val = state[i-1][j-1]
+      document.querySelector('.r'+i+'.c'+j+'>input').value = val>0?val:'';
+    }
+  }
 }
 
-document.getElementById("submitScore").addEventListener("click", function () {
-	let score = doneOrNot()? parseInt(1000000000/(new Date().getTime() - startTime)): 0;
+// MESSAGES FUNCTIONS
+function score() {
+  let score = done()? parseInt(1000000000/(new Date().getTime() - startTime)): 0;
   let msg = {
     "messageType": "SCORE",
     "score": score,
   };
   console.log(msg);
   window.parent.postMessage(msg, "*");
-});
+}
 
-document.getElementById("save").addEventListener("click", function () {
+function save() {
   let msg = {
     "messageType": "SAVE",
     "gameState": {
-      "board": pack(getBoard()),
+      "board": pack(board),
       "elapsedTime": new Date().getTime() - startTime,
     }
   };
   console.log(msg);
   window.parent.postMessage(msg, "*");
-});
+}
 
-document.getElementById("load").addEventListener("click", function () {
+function load() {
   var msg = {
     "messageType": "LOAD_REQUEST",
   };
   console.log(msg);
   window.parent.postMessage(msg, "*");
-});
+}
 
-window.addEventListener("message", function(evt) {
+function message(evt) {
   if(evt.data.messageType === "LOAD") {
-   	let board = evt.data.gameState.board;
+    let board = evt.data.gameState.board;
     let elapsedTime = evt.data.gameState.elapsedTime;
     resume(unpack(board));
     startTime = new Date().getTime() - elapsedTime;
   } else if (evt.data.messageType === "ERROR") {
     alert(evt.data.info);
   }
+}
+
+//LISTENERS
+document.getElementById("save").addEventListener("click", save);
+document.getElementById("load").addEventListener("click", load);
+window.addEventListener("message", message);
+document.getElementById("sudoku").addEventListener("change", function (e) {
+  let classes = e.target.parentElement.className;
+  let r = classes.match(/r[0-9]/)[0].match(/[0-9]/)-1;
+  let c = classes.match(/c[0-9]/)[0].match(/[0-9]/)-1;
+  board[r][c] = e.target.value;
+  if(done()){
+    score();
+  }
 });
 
-window.parent.postMessage({
-  messageType: "SETTING",
-  options: {
-  	"width": 600,
-    "height": 643
-    }
-}, "*");
-
-
-document.querySelector('#check').addEventListener('click', doneOrNot, false);
-
+//INITIALIZATION
 init()

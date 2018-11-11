1b0416afedef7b07702462b45a67b4b07bbe74cb
Marco Zugliani <Marco-Z@users.noreply.github.com>
2018-11-11 13:02:30 +0200

    Update index.html

--- comparison/static/js/timeline.js
+++ comparison/static/js/timeline.js
@@ -16,59 +16,103 @@ var COLORS = {
     'motorized/road/car': 'rgb(5, 86, 121)',
 };
 
-var start = null;
-var stop = null;
-
 Vue.component('timeline', {
-    template: ` \
-    <div class="timeline"> \
-        <p>{{ name }} <button class="delButton" v-on:click="$emit('deleteMe', name)"> x </button></p>\
+    template: `
+    <div class="timeline">
+        <p>{{ name }} <button class="delButton" v-on:click="$emit('deleteMe', name)"> x </button></p>
+        <div class="legs"></div>
     </div>`,
     props: ['legList', 'name'],
     mounted: function () {
         var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);        
-        if(!start){
-            start = this.legList.features[0].properties.time_start;
+        if (!this.$parent.start) {
+            this.$parent.start = this.legList.features[0].properties.time_start;
         }
-        if(!stop){
-            stop = this.legList.features[this.legList.features.length - 1].properties.time_stop;
+        if (!this.$parent.stop) {
+            this.$parent.stop = this.legList.features[this.legList.features.length - 1].properties.time_stop;
         }
-        var total = stop - start;
+        var total = this.$parent.stop - this.$parent.start;
         this.legList.features.forEach(leg => {
-            var legStart = (leg.properties.time_start - start) / total * w;
-            var legStop = (leg.properties.time_stop - start) / total * w;
+            var legStart = (leg.properties.time_start - this.$parent.start) / total * w;
+            var legStop = (leg.properties.time_stop - this.$parent.start) / total * w;
             var legElement = document.createElement("div");
-            legElement.style.width = (legStop - legStart) +"px";
+            legElement.style.width = (legStop - legStart) + "px";
             legElement.className = "leg";
             legElement.style.left = legStart + "px";
             legElement.style.background = COLORS[leg.properties.activity];
             var startTime = new Date(parseInt(leg.properties.time_start));
             var stopTime = new Date(parseInt(leg.properties.time_stop));
             legElement.title = leg.properties.activity + ": " + startTime.toLocaleTimeString() + "-" + stopTime.toLocaleTimeString()
-            this.$el.appendChild(legElement)
+            this.$el.querySelector(".legs").appendChild(legElement)
         });
     }
 });
 
 
+
 var timelines = new Vue({
     data: {
         sources: [],
+        start: null,
+        stop: null,
+        startPX: null,
     },
     el: 'timelines',
-    template: ` \
-    <div id="timelines"> \
-        <timeline v-for="s in sources" \
-                v-bind:legList="s" \
-                v-bind:name="s.name" \
-                v-on:deleteMe="deleteSource">\
-        </timeline> \
+    template: `
+    <div id="timelines">
+        <timeline v-for="s in sources"
+                v-bind:legList="s"
+                v-bind:name="s.name"
+                v-on:deleteMe="deleteSource">
+                v-on:startStop="setStartStop">
+        </timeline>
+        <div class="range" v-if="start">
+            <hr>
+            <span>{{ new Date(parseInt(start)).toLocaleString() }}</span>
+            <span class="right">{{ new Date(parseInt(stop)).toLocaleString() }}</span>
+        </div>
     </div>`,
     methods: {
         deleteSource: function(name) {
             this.sources = this.sources.filter(function (el) {
                 return el.name != name;
             });
+        },
+        startResize: function (event) {
+            console.log(event.target);
+            console.log(event);
+            console.log(document.querySelector(".dragArea"));
+            var dragArea = document.querySelector(".dragArea");
+
+            dragArea.style.left = event.clientX + 'px';
+            dragArea.style.width = '0px';
+            this.startPX = event.clientX;
+            document.addEventListener('mousemove', resizeDragArea);
+        },
+        resize: function (event) {
+            console.log(event.target);
+            console.log(event);
+            var dragArea = document.querySelector(".dragArea");
+
+            var scaleX = document.body.clientWidth / parseInt(dragArea.style.width.split("px")[0]);
+            var translationX = -parseInt(dragArea.style.left.split("px")[0]);
+            console.log("matrix(" + scaleX + "0,0,1," + translationX + ",0)");
+            document.querySelectorAll(".legs").forEach(function (leg) {
+                leg.style.transform = "matrix(" + scaleX + ",0,0,1," + translationX + ",0)";
+            })
+            document.removeEventListener('mousemove', resizeDragArea);
+
         }
     }
 });
+
+function resizeDragArea(event) {
+    console.log(event.clientX - timelines.startPX);
+    var w = event.clientX - timelines.startPX;
+    var dragArea = document.querySelector(".dragArea");
+    if(w < 0) {
+        document.querySelector(".dragArea").style.left = (timelines.startPX + w) + 'px';
+    }
+    dragArea.style.width = Math.abs(w) + 'px';
+
+}

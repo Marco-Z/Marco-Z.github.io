e3c129798672b0304d1f73a551769ee1afa02ae1
Your Name <you@example.com>
2019-07-28 14:24:38 +0200

    bootstrap quadrolingo

---
+++ quadrolingo/precache-manifest.b22c3a4d06d069e701540e6108e4a03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b699341c3053542c89ea019fe737a558",
+    "url": "/quadrolingo/index.html"
+  },
+  {
+    "revision": "03069d37c617e385f9c9",
+    "url": "/quadrolingo/static/css/main.28adf825.chunk.css"
+  },
+  {
+    "revision": "3e6e8d203bb4afe5a35d",
+    "url": "/quadrolingo/static/js/2.b10d7efa.chunk.js"
+  },
+  {
+    "revision": "03069d37c617e385f9c9",
+    "url": "/quadrolingo/static/js/main.948cefb7.chunk.js"
+  },
+  {
+    "revision": "9929e11d4d16254e4ed7",
+    "url": "/quadrolingo/static/js/runtime~main.3e706d02.js"
+  }
+]);

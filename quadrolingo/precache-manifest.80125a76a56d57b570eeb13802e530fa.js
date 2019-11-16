b6d5ac94dab9463d49a59e1d26238a50f241c60d
Your Name <you@example.com>
2019-11-16 19:05:32 +0100

    Update Pinyin API

---
+++ quadrolingo/precache-manifest.80125a76a56d57b570eeb13802e530fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464924b42e04009792ed56c5ca3eade0",
+    "url": "/quadrolingo/index.html"
+  },
+  {
+    "revision": "37d3a8a9f09f17e4f55c",
+    "url": "/quadrolingo/static/css/main.28adf825.chunk.css"
+  },
+  {
+    "revision": "3e6e8d203bb4afe5a35d",
+    "url": "/quadrolingo/static/js/2.b10d7efa.chunk.js"
+  },
+  {
+    "revision": "37d3a8a9f09f17e4f55c",
+    "url": "/quadrolingo/static/js/main.e5ff86ac.chunk.js"
+  },
+  {
+    "revision": "9929e11d4d16254e4ed7",
+    "url": "/quadrolingo/static/js/runtime~main.3e706d02.js"
+  }
+]);
